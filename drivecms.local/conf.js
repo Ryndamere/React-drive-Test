b52df803af974784900af836a78956d16637fb86
Ryndamere <44561375+Ryndamere@users.noreply.github.com>
2019-09-17 12:11:19 -0500

    Adding files that should all work
    please work

---
+++ drivecms.local/conf.js
@@ -0,0 +1,8 @@
+export default {
+    "author": "React Drive CMS",
+    "dashboardId": "10YY8rtmw29OEteiqrybcVwUYMOwUwQgPcFJ5l8HyET4",
+    "sendContactMessageUrlId": "AKfycbxHtB6vazHyTnSx5y6S-o3NrIuHXeQcOlX2lvyaY4H5bNPKbq8",
+    "shortname": "easydrivecms",
+    "root": "react-drive-cms",
+    "local": "drivecms.local"
+}
