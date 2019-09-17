6894ea92e2f27867c2d467d537745b903ddd2b55
Ryndamere <44561375+Ryndamere@users.noreply.github.com>
2019-09-17 14:19:35 -0500

    Update conf.js

--- drivecms.local/conf.js
+++ drivecms.local/conf.js
@@ -3,6 +3,6 @@ export default {
     "dashboardId": "10YY8rtmw29OEteiqrybcVwUYMOwUwQgPcFJ5l8HyET4",
     "sendContactMessageUrlId": "AKfycbxHtB6vazHyTnSx5y6S-o3NrIuHXeQcOlX2lvyaY4H5bNPKbq8",
     "shortname": "easydrivecms",
-    "root": "react-drive-cms",
+    "root": "React-drive-Test",
     "local": "drivecms.local"
 }
