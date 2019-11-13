6f95d12731889f31209fe0921b73465fa0abe431
Ryndamere <44561375+Ryndamere@users.noreply.github.com>
2019-11-13 17:41:40 -0600

    Update article.js

--- drivecms.local/routes/article.js
+++ drivecms.local/routes/article.js
@@ -252,7 +252,8 @@ let styles = StyleSheet.create({
         maxWidth: '100%'
     },
     mainNarrow: {
-        '@media (min-width: 768px)': {
+        width:'100%',
+        /*'@media (min-width: 768px)': {
             width: '60%'
         },
         '@media (min-width: 992px)': {
@@ -260,7 +261,7 @@ let styles = StyleSheet.create({
         },
         '@media (min-width: 1200px)': {
             width: '75%'
-        }
+        }*/
     },
     content: {
         display: 'block',
